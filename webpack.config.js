159d5aeb1a51c99e57223ace69cf9ce05828e708
a-kataoka <a-kataoka@cookbiz.jp>
2020-01-16 13:05:48 +0900

    use uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist');
 
@@ -60,5 +61,17 @@ module.exports = {
       new MiniCssExtractPlugin({
         filename: '[name].[hash].css'
       })
-  ]
+  ],
+  // 最適化
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+        ie8: true,
+        compress: {
+          // consoleを削除
+          drop_console: true
+        }
+      }
+    })]
+  }
 };
